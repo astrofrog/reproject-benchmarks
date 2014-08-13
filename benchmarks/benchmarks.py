c180a820ac252c94816208af0f7953de6f6c7b5e
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-08-13 14:38:13 +0200

    Fixed benchmarks

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,36 +1,49 @@
 # Write the benchmarking functions here.
 # See "Writing benchmarks" in the asv docs for more information.
 
+import os
+
+import numpy as np
+
+from astropy.wcs import WCS
+from astropy.io import fits
+
+from reproject import reproject
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
 
 class TimeSuite:
     """
     An example benchmark that times the performance of various kinds
     of iterating over dictionaries in Python.
     """
+
     def setup(self):
-        self.d = {}
-        for x in range(500):
-            self.d[x] = None
 
-    def time_keys(self):
-        for key in self.d.keys():
-            pass
+        self.header_in = fits.Header.fromtextfile(os.path.join(ROOT, 'data/gc_ga.hdr'))
+        self.header_out = fits.Header.fromtextfile(os.path.join(ROOT, 'data/gc_eq.hdr'))
 
-    def time_iterkeys(self):
-        for key in self.d.iterkeys():
-            pass
+        self.header_out_size = self.header_out.copy()
+        self.header_out_size['NAXIS'] = 2
+        self.header_out_size['NAXIS1'] = 600
+        self.header_out_size['NAXIS2'] = 550
 
-    def time_range(self):
-        d = self.d
-        for key in range(500):
-            x = d[key]
+        self.array_in = np.ones((700, 690))
 
-    def time_xrange(self):
-        d = self.d
-        for key in xrange(500):
-            x = d[key]
+        self.hdu_in = fits.ImageHDU(self.array_in, self.header_in)
 
+        self.wcs_in = WCS(self.header_in)
+        self.wcs_out = WCS(self.header_out)
+        self.shape_out = (600, 550)
 
-class MemSuite:
-    def mem_list(self):
-        return [0] * 256
+    def time_bilinear(self):
+        reproject(self.hdu_in, self.header_out_size, projection_type='bilinear')
+
+    def time_flux_conserving(self):
+        reproject(self.hdu_in, self.header_out_size, projection_type='flux-conserving')
+
+    def mem_bilinear(self):
+        reproject(self.hdu_in, self.header_out_size, projection_type='bilinear')
+
+    def mem_flux_conserving(self):
+        reproject(self.hdu_in, self.header_out_size, projection_type='flux-conserving')
