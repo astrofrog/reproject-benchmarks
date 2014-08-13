712ae447a32ca3905792e014292f70d6c1a6bbe1
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-08-13 15:08:40 +0200

    Updated benchmarks

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -2,6 +2,7 @@
 # See "Writing benchmarks" in the asv docs for more information.
 
 import os
+import warnings
 
 import numpy as np
 
@@ -20,30 +21,26 @@ class TimeSuite:
 
     def setup(self):
 
+        warnings.simplefilter('ignore')
+
         self.header_in = fits.Header.fromtextfile(os.path.join(ROOT, 'data/gc_ga.hdr'))
         self.header_out = fits.Header.fromtextfile(os.path.join(ROOT, 'data/gc_eq.hdr'))
 
         self.header_out_size = self.header_out.copy()
         self.header_out_size['NAXIS'] = 2
-        self.header_out_size['NAXIS1'] = 600
-        self.header_out_size['NAXIS2'] = 550
+        self.header_out_size['NAXIS1'] = 300
+        self.header_out_size['NAXIS2'] = 250
 
-        self.array_in = np.ones((700, 690))
+        self.array_in = np.ones((210, 215))
 
         self.hdu_in = fits.ImageHDU(self.array_in, self.header_in)
 
         self.wcs_in = WCS(self.header_in)
         self.wcs_out = WCS(self.header_out)
-        self.shape_out = (600, 550)
+        self.shape_out = (300, 250)
 
     def time_bilinear(self):
         reproject(self.hdu_in, self.header_out_size, projection_type='bilinear')
 
     def time_flux_conserving(self):
-        reproject(self.hdu_in, self.header_out_size, projection_type='flux-conserving')
-
-    def mem_bilinear(self):
-        reproject(self.hdu_in, self.header_out_size, projection_type='bilinear')
-
-    def mem_flux_conserving(self):
         reproject(self.hdu_in, self.header_out_size, projection_type='flux-conserving')
